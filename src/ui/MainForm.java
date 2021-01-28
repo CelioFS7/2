3b98676407bc5d4d8bbaad3a18b2c4a775c4bdfa
Celiofs7 <celioferreiramlm@gmail.com>
2021-01-28 14:50:40 -0300

    Agenda IJ

--- src/ui/MainForm.java
+++ src/ui/MainForm.java
@@ -4,6 +4,8 @@ import business.ContactBusiness;
 import entity.ContatoEntity;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -19,6 +21,8 @@ public class MainForm extends JFrame {
     private JLabel labelContactCount;
 
     private ContactBusiness mContacBusiness;
+    private String mName = "";
+    private String mfone = "";
 
     public MainForm() {
 
@@ -39,23 +43,23 @@ public class MainForm extends JFrame {
     }
 
     private void loadContacts() {
-       List<ContatoEntity> contactlist = mContacBusiness.getList();
-        String[] columnNames= {"Nomel, Telefone"};
-        DefaultTableModel model= new DefaultTableModel(new Object[0][0], columnNames);
+        List<ContatoEntity> contactlist = mContacBusiness.getList();
+        String[] columnNames = {"Nomel, Telefone"};
+        DefaultTableModel model = new DefaultTableModel(new Object[0][0], columnNames);
 
 
-            for (ContatoEntity i: contactlist){
-                Object [] o = new Object[2];
+        for (ContatoEntity i : contactlist) {
+            Object[] o = new Object[2];
 
-                o[0] = i.getName();
-                o[1] = i.getFone();
+            o[0] = i.getName();
+            o[1] = i.getFone();
 
-                model.addRow(o);
-            }
-            tableContatos.clearSelection();
-            tableContatos.setModel(model);
+            model.addRow(o);
+        }
+        tableContatos.clearSelection();
+        tableContatos.setModel(model);
 
-            labelContactCount.setText(mContacBusiness.getContactCountDescription());
+        labelContactCount.setText(mContacBusiness.getContactCountDescription());
     }
 
     private void setListeners() {
@@ -67,11 +71,38 @@ public class MainForm extends JFrame {
             }
         });
 
+        tableContatos.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if (e.getValueIsAdjusting()) {
+
+                    if (tableContatos.getSelectedRow() != -1) {
+
+                        mName = tableContatos.getValueAt(tableContatos.getSelectedRow(), 0).toString();
+                        mfone = tableContatos.getValueAt(tableContatos.getSelectedRow(), 1).toString();
+
+
+                    }
+                }
+            }
+        });
+
         buttonRemover.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                try {mContacBusiness.delete(mName, mfone);
+                    loadContacts();
+
+                    mName ="";
+                    mfone ="";
+
+                }catch (Exception excp){
+                    JOptionPane.showMessageDialog(new JFrame(),excp.getMessage());
+                }
             }
         });
+
+
     }
 }
